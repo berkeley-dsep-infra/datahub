e30b282cfe7464168d081d2aec0fe3b04abc6650
ryanlovett <rylo@berkeley.edu>
2017-08-07 15:50:52 -0700

    Install libraries/data for LS88-5 (text analysis).

---
+++ datahub/user/connectors/text.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# We put NLTK data under /opt/conda, and then set the NLTK_DATA
+# environment variable appropriately. For more info see
+# http://www.nltk.org/data.html#command-line-installation
+## tbd: Dockerfile: ENV NLTK_DATA /usr/share/nltk_data
+mkdir /usr/share/nltk_data
+
+# MODEL_____________________  TERMS____
+# averaged_perceptron_tagger: 17sp 16fa
+#                    cmudict:      16fa
+#          maxent_ne_chunker: 17sp 16fa
+#                      punkt: 17sp 16fa
+#                  stopwords: 17sp
+#                    wordnet: 17sp 16fa
+#                      words: 17sp 16fa
+python -m nltk.downloader -d /usr/share/nltk_data \
+    averaged_perceptron_tagger \
+    cmudict \
+    maxent_ne_chunker \
+    punkt \
+    stopwords \
+    wordnet \
+    words \
+    ;
