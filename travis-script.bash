333988410dbb57de425b313b3adb580f04a98e14
yuvipanda <yuvipanda@gmail.com>
2017-08-09 19:47:51 -0700

    Don't wait on host key check in deploy script

--- travis-script.bash
+++ travis-script.bash
@@ -18,6 +18,8 @@ elif [[ ${ACTION} == 'deploy' ]]; then
     CHECKOUT_DIR="/tmp/${TRAVIS_BUILD_NUMBER}"
     COMMIT="${TRAVIS_COMMIT}"
     MASTER_HOST="datahub-fa17-${TRAVIS_BRANCH}.westus2.cloudapp.azure.com"
-    ssh -i sshkey datahub@${MASTER_HOST} \
+    ssh -i sshkey \
+        -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
+        datahub@${MASTER_HOST} \
         "git clone ${REPO} ${CHECKOUT_DIR} && cd ${CHECKOUT_DIR} && git checkout ${COMMIT} && ./deploy.py deploy datahub"
 fi
