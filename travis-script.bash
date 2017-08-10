b771824a0066fdc8bdccb2fb1d8085842559bd82
yuvipanda <yuvipanda@gmail.com>
2017-08-09 20:32:16 -0700

    Unlock git-crypt before deploying

--- travis-script.bash
+++ travis-script.bash
@@ -22,9 +22,10 @@ elif [[ ${ACTION} == 'deploy' ]]; then
 
     openssl aes-256-cbc -K $encrypted_eee6172de5ea_key -iv $encrypted_eee6172de5ea_iv -in sshkey.enc -out sshkey -d
     chmod 0400 sshkey
+
     ssh -i sshkey \
         -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
         datahub@${MASTER_HOST} \
-        "git clone ${REPO} ${CHECKOUT_DIR} && cd ${CHECKOUT_DIR} && git checkout ${COMMIT} && ./deploy.py deploy datahub"
+        "git clone ${REPO} ${CHECKOUT_DIR} && cd ${CHECKOUT_DIR} && git crypt unlock /etc/deploy-secret-keyfile && git checkout ${COMMIT} && ./deploy.py deploy datahub; rm -rf ${CHECKOUT_DIR}"
     rm -rf sshkey
 fi
