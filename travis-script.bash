c8629af6fa8bce25de2c0bd77760f859e338bcf4
yuvipanda <yuvipanda@gmail.com>
2017-08-09 19:55:31 -0700

    Move ssh key decryption to deploy script

--- travis-script.bash
+++ travis-script.bash
@@ -18,8 +18,12 @@ elif [[ ${ACTION} == 'deploy' ]]; then
     CHECKOUT_DIR="/tmp/${TRAVIS_BUILD_NUMBER}"
     COMMIT="${TRAVIS_COMMIT}"
     MASTER_HOST="datahub-fa17-${TRAVIS_BRANCH}.westus2.cloudapp.azure.com"
+
+
+    openssl aes-256-cbc -K $encrypted_eee6172de5ea_key -iv $encrypted_eee6172de5ea_iv -in sshkey.enc -out sshkey -d
     ssh -i sshkey \
         -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
         datahub@${MASTER_HOST} \
         "git clone ${REPO} ${CHECKOUT_DIR} && cd ${CHECKOUT_DIR} && git checkout ${COMMIT} && ./deploy.py deploy datahub"
+    rm -rf sshkey
 fi
