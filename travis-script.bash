2483c1c28a126881e0c974615cbc99d68b3f2e25
yuvipanda <yuvipanda@gmail.com>
2017-08-09 20:00:23 -0700

    Fix sshkey permissions

--- travis-script.bash
+++ travis-script.bash
@@ -21,6 +21,7 @@ elif [[ ${ACTION} == 'deploy' ]]; then
 
 
     openssl aes-256-cbc -K $encrypted_eee6172de5ea_key -iv $encrypted_eee6172de5ea_iv -in sshkey.enc -out sshkey -d
+    chmod 0400 sshkey
     ssh -i sshkey \
         -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
         datahub@${MASTER_HOST} \
