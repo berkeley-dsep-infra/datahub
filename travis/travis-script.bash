eba3602aad5a5df979376eec8a28ee676f26cbb9
yuvipanda <yuvipanda@gmail.com>
2017-08-14 17:39:07 -0700

    Fix remaining references to plain 'sshkey'

--- travis/travis-script.bash
+++ travis/travis-script.bash
@@ -24,13 +24,13 @@ elif [[ ${ACTION} == 'deploy' ]]; then
 
     echo "Fetching ssh key..."
     openssl aes-256-cbc -K $encrypted_eee6172de5ea_key -iv $encrypted_eee6172de5ea_iv -in "${SSHKEY}.enc" -out "${SSHKEY}" -d
-    chmod 0400 sshkey
+    chmod 0400 "${SSHKEY}"
 
     echo "SSHing..."
     ssh -i "${SSHKEY}"\
         -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
         datahub@${MASTER_HOST} \
         "git clone ${REPO} ${CHECKOUT_DIR} && cd ${CHECKOUT_DIR} && git checkout ${COMMIT} && git crypt unlock /etc/deploy-secret-keyfile && ./deploy.py deploy ${TRAVIS_BRANCH}; rm -rf ${CHECKOUT_DIR}"
-    rm -rf sshkey
+    rm -rf "${SSHKEY}"
     echo "Done!"
 fi
