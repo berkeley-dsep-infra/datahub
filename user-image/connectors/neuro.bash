e35cf30e1a9e02e2fbf8d4fe49a4bcc61545bd1f
Gunjan Baid <gunjanbaid@users.noreply.github.com>
2017-10-20 22:28:56 -0700

    Using older version of cython, should work with hmms.

--- user-image/connectors/neuro.bash
+++ user-image/connectors/neuro.bash
@@ -5,7 +5,7 @@ set -euo pipefail
 jupyter serverextension enable  --sys-prefix --py nbserverproxy
 
 # pycortex seems to need cython, but isn't explicitly defined as a dependency
-pip install --no-cache-dir cython==0.26
+pip install --no-cache-dir cython==0.25.2
 
 # pycortex can't be installed from the repository at the moment
 # ${CONDA_DIR}/bin/pip install git+https://github.com/gallantlab/pycortex@data8
