728b9c782094ee4e59a7b1d6411742b6d97206f9
Gunjan Baid <gunjanbaid@users.noreply.github.com>
2017-10-25 22:56:55 -0700

    Addressing reviewer comments.

--- user-image/connectors/neuro.bash
+++ user-image/connectors/neuro.bash
@@ -4,10 +4,6 @@ set -euo pipefail
 # nbserver; installed via requirements.txt
 jupyter serverextension enable  --sys-prefix --py nbserverproxy
 
-# pycortex seems to need cython, but isn't explicitly defined as a dependency
-# pip install --no-cache-dir cython==0.26
-# cython 0.25.2 installed in Dockerfile		--Gunjan
-
 # pycortex can't be installed from the repository at the moment
 # ${CONDA_DIR}/bin/pip install git+https://github.com/gallantlab/pycortex@data8
 
