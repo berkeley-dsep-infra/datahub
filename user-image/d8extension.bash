a24ace6eeeca9ae69e82d5584309306e849ab6fc
Adeel Cheema <cheemaadeel@gmail.com>
2018-03-16 11:26:40 -0700

    Adding scratchpad nbextension

---
+++ user-image/d8extension.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd tmp
+git clone git://github.com/minrk/nbextension-scratchpad
+cd nbextension-scratchpad/
+git checkout e92fa23
+cd ..
+jupyter nbextension install --sys-prefix nbextension-scratchpad
+jupyter nbextension enable  --sys-prefix nbextension-scratchpad/main
+cd .. 
+rm -rf /tmp/nbextension-scratchpad
