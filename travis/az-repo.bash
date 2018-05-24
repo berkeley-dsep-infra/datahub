954188e22ecd621be65064bc24ad46baeb8c195f
ryanlovett <rylo@berkeley.edu>
2018-05-24 12:59:28 -0700

    Add new Microsoft signing key.

--- travis/az-repo.bash
+++ travis/az-repo.bash
@@ -8,6 +8,10 @@ echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy ma
 
 sudo apt-key adv --keyserver packages.microsoft.com \
 	--recv-keys 52E16F86FEE04B979B07E28DB02C46DF417A0893
+
+# new as of 5/2018
+curl -L https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+
 sudo apt-get install apt-transport-https
 sudo apt-get update
 sudo apt-get install azure-cli
