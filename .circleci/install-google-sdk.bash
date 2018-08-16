f4a2d881deb0f305a53767013670b1005a63cf30
yuvipanda <yuvipanda@gmail.com>
2018-08-15 21:23:13 -0600

    Download & cache google cloud sdk
    Also try some ptah fiddling

--- .circleci/install-google-sdk.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Install google cloud SDK + kubectl
-set -euo pipefail
-
-export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
-echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" > /etc/apt/sources.list.d/google-cloud-sdk.list
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-apt-get update --yes
-apt-get install --yes google-cloud-sdk kubectl
