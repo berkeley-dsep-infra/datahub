b958f7a98f2e55321b7dc432cec54cf209d9a525
ryanlovett <rylo@berkeley.edu>
2018-08-27 14:29:24 -0700

    Disable failing libraries.

--- deployments/datahub/image/connectors/cp255.bash
+++ deployments/datahub/image/connectors/cp255.bash
@@ -20,13 +20,16 @@ cd ~ && rm -rf datashader
 pip install geojson==2.4.0
 pip install holoviews==1.10.7
 pip install ipyleaflet==0.9.0
+# osmnx bumps matplotlib 2.0.0 > 2.2.3; networkx 1.11 > 2.1
 pip install OSMnx==0.8.1
 pip install pandana==0.3.0
-pip install plotly==1.39.4
 pip install shapely==1.6.4
 pip install TwitterAPI==2.5.4
 pip install urbanaccess==0.1.0
-pip install urbansim==3.1.1
+
+git clone --branch v3.1.1 https://github.com/UDST/urbansim.git
+cd ~/urbanism && python setup.py install
+rm -rf choicemodels
 
 # GSI asked for these but we need different versions for other courses
 # pip install folium==0.6.0
@@ -35,6 +38,10 @@ pip install urbansim==3.1.1
 #pip install gdal==2.2.4
 # this fails; wants 1.5.{0,1,2,3}
 #pip install geoviews==1.4.3
+# there is no such version
+#pip install plotly==1.39.4
+# there is no such library on pypi
+#pip install urbansim==3.1.1
 
 # GSI asked for urbansim_templates but this causes problems
 # urbanism templates fails on osmnet
