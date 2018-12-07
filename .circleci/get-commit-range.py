70719244d225c387b09a11dd558a349768c5eef6
yuvipanda <yuvipanda@gmail.com>
2018-12-07 13:04:20 -0800

    Use a Python script to calculate COMMIT_RANGE
    Bash is too complicated when using non-forked PRs on
    Circle, since it doesn't give you a PR number at all when
    this is the case!!!!

---
+++ .circleci/get-commit-range.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+import os
+import argparse
+from github import Github
+
+
+def from_pr(project, repo, pr_number):
+    gh = Github()
+    pr = gh.get_repo(f'{project}/{repo}').get_pull(pr_number)
+    base = pr.base.ref
+    head = pr.head.ref
+    return f'origin/{base}...{head}'
+
+
+def main():
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument(
+        'project',
+        default=os.environ['CIRCLE_PROJECT_USERNAME'],
+        nargs='?'
+    )
+    argparser.add_argument(
+        'repo',
+        default=os.environ['CIRCLE_PROJECT_REPONAME'],
+        nargs='?'
+    )
+
+    argparser.add_argument(
+        '--pr-number',
+        type=int,
+        nargs='?'
+    )
+
+    args = argparser.parse_args()
+
+    if not args.pr_number:
+        pr_number = int(os.environ['CIRCLE_PR_NUMBER'])
+    else:
+        pr_number = args.pr_number
+    print(from_pr(args.project, args.repo, pr_number))
+
+
+if __name__ == '__main__':
+    main()
