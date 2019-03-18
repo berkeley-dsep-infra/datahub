e6e30fb34ba51a49cfe67086ec6d08a01475d44a
ryanlovett <rylo@berkeley.edu>
2019-03-18 10:09:49 -0700

    Install irkernel.

--- deployments/datahub/image/install.R
+++ deployments/datahub/image/install.R
@@ -61,3 +61,7 @@ devtools::install_github('cran/summarytools', ref = '8eeb20a', upgrade_dependenc
 
 # github ref is version 5.2.2
 devtools::install_github('cran/stargazer', ref = '736d303', upgrade_dependencies = FALSE)
+
+# github ref is version 0.8.15
+devtools::install_github('cran/irkernel', ref = '238b691', upgrade_dependencies = FALSE)
+IRkernel::installspec(user = FALSE, prefix='/srv/app/venv')
