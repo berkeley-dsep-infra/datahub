0366595b1431283fb09a6482415d6c1bc528a1af
Scott Lee <scottjlee@users.noreply.github.com>
2019-04-06 19:27:01 -0700

    Install execute_time Jupyter extension
    https://github.com/ipython-contrib/jupyter_contrib_nbextensions/tree/master/src/jupyter_contrib_nbextensions/nbextensions/execute_time#internals

--- deployments/datahub/image/d8extension.bash
+++ deployments/datahub/image/d8extension.bash
@@ -7,6 +7,11 @@ cd nbextension-scratchpad/
 git checkout e92fa23
 cd ..
 jupyter nbextension install --sys-prefix nbextension-scratchpad
-jupyter nbextension enable  --sys-prefix nbextension-scratchpad/main
+jupyter nbextension enable --sys-prefix nbextension-scratchpad/main
 cd .. 
+
+pip install jupyter_contrib_nbextensions
+jupyter contrib nbextension install --sys-prefix
+jupyter nbextension enable execute_time/ExecuteTime
+
 rm -rf /tmp/nbextension-scratchpad
