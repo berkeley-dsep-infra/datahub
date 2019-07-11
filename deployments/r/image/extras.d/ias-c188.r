5e9fee85d1f568285ceb39cdcc4fe6f6d78e8938
yuvipanda <yuvipanda@gmail.com>
2019-07-11 10:35:31 -0700

    Print what we're installing
    quiet=True seems to make *everything* quiet

--- deployments/r/image/extras.d/ias-c188.r
+++ deployments/r/image/extras.d/ias-c188.r
@@ -1,7 +1,17 @@
 #!/usr/bin/env Rscript
 # From https://github.com/berkeley-dsep-infra/datahub/issues/814
+print("Installing packages for IA C188")
 
+print("Installing stargazer...")
 devtools::install_github('cran/stargazer', ref='5.2.2', upgrade_dependencies=FALSE, quiet=TRUE)
+
+print("Installing lm.beta...")
 devtools::install_github('cran/lm.beta', ref='1.5-1', upgrade_dependencies=FALSE, quiet=TRUE)
+
+print("Installing multcomp...")
 devtools::install_github('cran/multcomp', ref='1.4-8', upgrade_dependencies=FALSE, quiet=TRUE)
+
+print("Installing lfe...")
 devtools::install_github('cran/lfe', ref='2.8-2', upgrade_dependencies=FALSE, quiet=TRUE)
+
+print("Done installing packages for IA C188")
