21d520a007bbe206db690db3c3ec021c2dc4b94e
yuvipanda <yuvipanda@gmail.com>
2019-07-11 10:19:20 -0700

    Pass quiet=True to install_github to see if it shuts up

--- deployments/r/image/extras.d/ias-c188.r
+++ deployments/r/image/extras.d/ias-c188.r
@@ -1,7 +1,7 @@
 #!/usr/bin/env Rscript
 # From https://github.com/berkeley-dsep-infra/datahub/issues/814
 
-devtools::install_github('cran/stargazer', ref='5.2.2', upgrade_dependencies=FALSE)
-devtools::install_github('cran/lm.beta', ref='1.5-1', upgrade_dependencies=FALSE)
-devtools::install_github('cran/multcomp', ref='1.4-8', upgrade_dependencies=FALSE)
-devtools::install_github('cran/lfe', ref='2.8-2', upgrade_dependencies=FALSE)
+devtools::install_github('cran/stargazer', ref='5.2.2', upgrade_dependencies=FALSE, quiet=TRUE)
+devtools::install_github('cran/lm.beta', ref='1.5-1', upgrade_dependencies=FALSE, quiet=TRUE)
+devtools::install_github('cran/multcomp', ref='1.4-8', upgrade_dependencies=FALSE, quiet=TRUE)
+devtools::install_github('cran/lfe', ref='2.8-2', upgrade_dependencies=FALSE, quiet=TRUE)
