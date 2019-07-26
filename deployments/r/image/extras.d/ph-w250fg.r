411becdfd6c3b3a7daa7dd3cd09ca32d164a067e
Jonathan Felder <felder@berkeley.edu>
2019-07-26 12:08:07 -0700

    Adding rlist, jsonlite, checkr, assertthat, dplyr, ggplot2, tidyr, reticulate

--- deployments/r/image/extras.d/ph-w250fg.r
+++ deployments/r/image/extras.d/ph-w250fg.r
@@ -5,4 +5,28 @@ print("Installing packages for PHW250F+G")
 print("Installing here...")
 devtools::install_github('cran/here', ref='0.1', upgrade_dependencies=FALSE, quiet=TRUE)
 
+print("Installing rlist...")
+devtools::install_github('cran/rlist', ref='0.4.6.1', upgrade_dependencies=FALSE, quiet=TRUE)
+
+print("Installing jsonlite...")
+devtools::install_github('cran/jsonlite', ref='1.6', upgrade_dependencies=FALSE, quiet=TRUE)
+
+print("Installing checkr...")
+devtools::install_github('cran/checkr', ref='0.5.0', upgrade_dependencies=FALSE, quiet=TRUE)
+
+print("Installing assertthat...")
+devtools::install_github('cran/assertthat', ref='0.2.0', upgrade_dependencies=FALSE, quiet=TRUE)
+
+print("Installing dplyr...")
+devtools::install_github('cran/dplyr', ref='0.8.1', upgrade_dependencies=FALSE, quiet=TRUE)
+
+print("Installing ggplot2...")
+devtools::install_github('cran/ggplot2', ref='3.1.0', upgrade_dependencies=FALSE, quiet=TRUE)
+
+print("Installing tidyr...")
+devtools::install_github('cran/tidyr', ref='0.8.3', upgrade_dependencies=FALSE, quiet=TRUE)
+
+print("Installing reticulate...")
+devtools::install_github('cran/reticulate', ref='1.13', upgrade_dependencies=FALSE, quiet=TRUE)
+
 print("Done installing packages for PHW250F+G")
