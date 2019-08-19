6f1b7efdcb2dd3c670fd29affba46b98893c9935
ryanlovett <rylo@berkeley.edu>
2019-08-19 10:51:16 -0700

    Print status. Include depends in class lib install.

--- deployments/r/image/class-libs.R
+++ deployments/r/image/class-libs.R
@@ -1,9 +1,9 @@
 #!/usr/bin/env Rscript
 
 class_libs_install_github <- function(class_name, class_libs) {
-    paste("Installing packages for", class_name)
+    print(paste("Installing packages for", class_name))
     for (i in seq(1, length(class_libs), 2)) {
-        paste("Installing", class_libs[i], class_libs[i+1])
+        print(paste("Installing", class_libs[i], class_libs[i+1]))
         devtools::install_github(
             class_libs[i], ref=class_libs[i+1],
             upgrade_dependencies=FALSE, quiet=TRUE
@@ -13,13 +13,13 @@ class_libs_install_github <- function(class_name, class_libs) {
 }
 
 class_libs_install_version <- function(class_name, class_libs) {
-    paste("Installing packages for", class_name)
+    print(paste("Installing packages for", class_name))
     for (i in seq(1, length(class_libs), 2)) {
-        paste("Installing", class_libs[i], class_libs[i+1])
+        print(paste("Installing", class_libs[i], class_libs[i+1]))
         devtools::install_version(
             class_libs[i], version = class_libs[i+1],
-            dependencies=FALSE, quiet=TRUE
+            dependencies=TRUE, quiet=TRUE
         )
     }
-    paste("Done installing packages for", class_name)
+    print(paste("Done installing packages for", class_name))
 }
