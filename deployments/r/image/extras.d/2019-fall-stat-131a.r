63be0462c8923f6c0fca29943d56fe4531290162
ryanlovett <rylo@berkeley.edu>
2019-08-19 10:53:11 -0700

    Remove explicit dependencies.

--- deployments/r/image/extras.d/2019-fall-stat-131a.r
+++ deployments/r/image/extras.d/2019-fall-stat-131a.r
@@ -9,18 +9,10 @@ class_libs = c(
   "DAAG", "1.22",
   "faraway", "1.0.7",
   "fdrtool", "1.2.15",
-  "barcode", "1.1", # dependency of gpairs
-  "vcd", "1.4-4",   # dependency of gpairs
   "gpairs", "1.2",
   "gplots", "3.0.1.1",
   "hexbin", "1.27.3",
   "leaps", "2.9",
-  "packagemaker", "0.27", # dependency of NMF
-  "registry", "0.5-1",    # dependency of NMF
-  "rngtools", "1.4",      # dependency of NMF
-  "gridBase", "0.4-7",    # dependency of NMF
-  "doParallel", "1.0.14", # dependency of NMF
-  "bibtex", "0.4-2",      # dependency of NMF
   "NMF", "0.21.0",
   "randomForest", "4.6-12",
   "RColorBrewer", "1.1-2",
