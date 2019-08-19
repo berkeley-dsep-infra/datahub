77561b84a080de53e1979f1f3b0f2cfed6e77fb5
ryanlovett <rylo@berkeley.edu>
2019-08-19 10:26:06 -0700

    Use install_version to avoid github API limits.
    Also install tcl/tk for vioplot.

--- deployments/r/image/extras.d/2019-fall-stat-131a.r
+++ deployments/r/image/extras.d/2019-fall-stat-131a.r
@@ -1,25 +1,34 @@
 #!/usr/bin/env Rscript
 
-source("/tmp/install-class-libs.R")
+source("/tmp/class-libs.R")
 
 class_name = "2019 Fall Stat 131a"
 class_libs = c(
-  "cran/alluvial", "0.1-2",
-  "cran/DAAG", "1.22",
-  "cran/faraway", "1.0.7",
-  "cran/fdrtool", "1.2.15",
-  "cran/gpairs", "1.2",
-  "cran/gplots", "3.0.1.1",
-  "cran/hexbin", "1.27.3",
-  "cran/leaps", "2.9",
-  "cran/NMF", "0.21.0",
-  "cran/randomForest", "4.6-12",
-  "cran/RColorBrewer", "1.1-2",
-  "cran/rjson", "0.2.20",
-  "cran/rpart.plot", "3.0.7",
-  "cran/scatterplot3d", "0.3-41",
-  "cran/shape", "1.4.4",
-  "cran/sm", "2.2-5.6",
-  "cran/vioplot", "0.3.2"
+  "alluvial", "0.1-2",
+  "latticeExtra", "0.6-28",
+  "DAAG", "1.22",
+  "faraway", "1.0.7",
+  "fdrtool", "1.2.15",
+  "barcode", "1.1", # dependency of gpairs
+  "vcd", "1.4-4",   # dependency of gpairs
+  "gpairs", "1.2",
+  "gplots", "3.0.1.1",
+  "hexbin", "1.27.3",
+  "leaps", "2.9",
+  "packagemaker", "0.27", # dependency of NMF
+  "registry", "0.5-1",    # dependency of NMF
+  "rngtools", "1.4",      # dependency of NMF
+  "gridBase", "0.4-7",    # dependency of NMF
+  "doParallel", "1.0.14", # dependency of NMF
+  "bibtex", "0.4-2",      # dependency of NMF
+  "NMF", "0.21.0",
+  "randomForest", "4.6-12",
+  "RColorBrewer", "1.1-2",
+  "rjson", "0.2.20",
+  "rpart.plot", "3.0.7",
+  "scatterplot3d", "0.3-41",
+  "shape", "1.4.4",
+  "sm", "2.2-5.6",
+  "vioplot", "0.3.0"
 )
-install_class_libs(class_name, class_libs)
+class_libs_install_version(class_name, class_libs)
