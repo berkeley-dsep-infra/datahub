e3c1acc646fd7f49679ccc52eb3d49b8e042bbcc
Corinne Riddell <corinne.riddell@gmail.com>
2019-12-02 16:22:50 -0800

    Add tigris for PH142

--- deployments/r/image/extras.d/ph-142.r
+++ deployments/r/image/extras.d/ph-142.r
@@ -38,4 +38,7 @@ devtools::install_github('cran/measurements', ref='1.3.0', upgrade_dependencies=
 print("Installing dagitty...")
 devtools::install_github('cran/dagitty', ref='0.2-2', upgrade_dependencies=FALSE, quiet=TRUE)
 
+print("Installing tigris...")
+devtools::install_github('cran/tigris', ref='0.8.2', upgrade_dependencies=FALSE, quiet=TRUE)
+
 print("Done installing packages for PH142")
