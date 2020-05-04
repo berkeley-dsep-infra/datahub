c0a192110d30a430b3e0ca6e64c3ee9c47831141
YuviPanda <yuvipanda@gmail.com>
2020-05-04 12:27:06 +0530

    Move NotebookNotary config to jupyter_notebook_config
    It was in IPython config earlier

--- deployments/prob140/image/jupyter_notebook_config.py
+++ deployments/prob140/image/jupyter_notebook_config.py
@@ -7,3 +7,10 @@ c.ServerProxy.servers = {
     }
   }
 }
+
+
+# Use memory for notebook notary file to workaround corrupted files on nfs
+# https://www.sqlite.org/inmemorydb.html
+# https://github.com/jupyter/jupyter/issues/174
+# https://github.com/ipython/ipython/issues/9163
+c.NotebookNotary.db_file = ":memory:"
