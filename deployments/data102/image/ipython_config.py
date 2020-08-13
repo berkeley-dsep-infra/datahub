3a505db61e1a9bb42d990246967d2c2a2d05f04c
YuviPanda <yuvipanda@gmail.com>
2020-08-13 16:46:42 +0530

    template: Fix typo that made ipython_config.py useless
    grr aargh. Fixes it in all images too.

--- deployments/data102/image/ipython_config.py
+++ deployments/data102/image/ipython_config.py
@@ -1,7 +1,7 @@
 # Disable sqlite things since we use nfs
 
 # history manager -- we don't really use it
-c.Historymanager.enabled = False
+c.HistoryManager.enabled = False
 
 # nbformat signing. from nbformat/tests/test_sign.py
 from nbformat import sign
