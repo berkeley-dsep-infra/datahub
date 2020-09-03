0cf1fff49e01d8e9b715652f63fcb92519e32aaf
Ryan Lovett <rylo@xt99.attlocal.net>
2020-09-03 13:00:47 -0700

    Update tigris, and remove quiet.

--- deployments/r/image/extras.d/ph-142.r
+++ deployments/r/image/extras.d/ph-142.r
@@ -26,13 +26,13 @@ print("Installing rlang...")
 devtools::install_github('cran/rlang', ref='0.4.6', upgrade='never', quiet=FALSE)
 
 print("Installing tigris...")
-devtools::install_github('walkerke/tigris', ref='78d1e44ccc1f561342078c0e79d0415fa4396389', upgrade='never', quiet=TRUE)
+devtools::install_github('walkerke/tigris', ref='3d50076', upgrade='never')
 
 print("Installing cowplot...")
 devtools::install_github('cran/cowplot', ref='1.0.0', upgrade='never')
 
 # print("Installing patchwork...")
-devtools::install_github('thomasp85/patchwork', ref='36b4918', upgrade='never', quiet=TRUE)
+devtools::install_github('thomasp85/patchwork', ref='36b4918', upgrade='never')
 
 print("Installing googlesheets4")
 devtools::install_github("tidyverse/googlesheets4", ref='9348a37511c45257f2aceadf76674a601740e708', upgrade='never', quiet=TRUE)
