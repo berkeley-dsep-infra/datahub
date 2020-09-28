62b1fceb291d52a9bfe3dce5f4e33d447bbf3a4e
psudmant <psudmant@mit.edu>
2020-09-27 21:00:27 -0700

    added install.R to manage R packages

---
+++ deployments/biology/image/install.R
@@ -0,0 +1,4 @@
+# R packages to be installed that aren't from apt
+
+#devtools::install_github('cran/tidyverse', ref = 'bd9ff0b', upgrade_dependencies = FALSE)
+devtools::install_version('tidyverse', '1.2.1', upgrade_dependencies = FALSE)
