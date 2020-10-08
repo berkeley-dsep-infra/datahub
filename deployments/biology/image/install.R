5c4fa745faa5a252e5133daf70004d793f3441ba
psudmant <psudmant@mit.edu>
2020-10-07 19:16:37 -0700

    remove unused packages

--- deployments/biology/image/install.R
+++ deployments/biology/image/install.R
@@ -8,21 +8,6 @@ install.packages("devtools")
 # so refer to `installed.packages()` for authoritative list
 cran_packages <- c(
   "tidyverse", "1.3.0"
-  #"learnr", "0.10.1",
-  #"knitr", "1.29"
-  #"rmarkdown", "2.3",
-  #"Rcpp", "1.0.5"
-  #"reticulate", "1.16",
-  #"openintro", "2.0.0",
-  #"gridExtra", "2.3",
-  #"BHH2", "2016.05.31",
-  #"nycflights13", "1.0.1",
-  #"tinytex", "0.25",
-  #"spdep", "1.1-5",
-  #"shiny", "1.5.0",
-  #"rstan", "2.21.2",
-  #"ggforce", "0.3.2",
-  #"ggthemes", "4.2.0"
 )
 
 #github_packages <- c(
