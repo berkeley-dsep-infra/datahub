9d261127f9f52467b88d427dd2fef13e6ce39403
Jonathan Felder <felder@berkeley.edu>
2021-08-19 11:45:54 -0700

    Grabbing 0.12.0 from github which is the latest tagged release, issue #2556

--- deployments/publichealth/image/r-packages/ph-252.r
+++ deployments/publichealth/image/r-packages/ph-252.r
@@ -6,9 +6,4 @@ print("Installing packages for PH 252")
 source("/tmp/class-libs.R")
 
 class_name = "PH 252"
-
-class_libs = c(
-  "tableone", "0.13.0"
-)
-
-class_libs_install_version(class_name, class_libs)
+devtools::install_github('kaz-yos/tableone', ref='7cfcd71')
