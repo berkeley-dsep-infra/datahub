79dba86031c3b5e4b7427d44ed33a59a4ce0ab9b
Jonathan Felder <felder@berkeley.edu>
2021-08-20 16:20:26 -0700

    Installing epi and foreign for issue #2524

--- deployments/publichealth/image/r-packages/ph-252.r
+++ deployments/publichealth/image/r-packages/ph-252.r
@@ -1,9 +1,20 @@
 #!/usr/bin/env Rscript
 # For https://github.com/berkeley-dsep-infra/datahub/issues/2556
+# For https://github.com/berkeley-dsep-infra/datahub/issues/2524
 # Fall 2021
 print("Installing packages for PH 252")
 
 source("/tmp/class-libs.R")
 
 class_name = "PH 252"
-devtools::install_github('kaz-yos/tableone', ref='7cfcd71')
+
+class_libs = c(
+  "epi", "2.44",
+  "foreign", "0.8-81"
+)
+
+devtools::install_github('kaz-yos/tableone', ref='7cfcd71', upgrade_dependencies=FALSE, quiet=FALSE)
+
+class_libs_install_version(class_name, class_libs)
+
+print("Done installing packages for PH 252")
