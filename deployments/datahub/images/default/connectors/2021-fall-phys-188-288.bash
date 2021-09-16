bc43d15a8d7a1369ee4dbba78a2e784143752bfb
Balaji <balajialwar@BERKELEY.EDU>
2021-09-16 15:20:31 -0700

    Making edits for avoiding warning messages

--- deployments/datahub/images/default/connectors/2021-fall-phys-188-288.bash
+++ deployments/datahub/images/default/connectors/2021-fall-phys-188-288.bash
@@ -4,5 +4,5 @@ set -euo pipefail
 cd ${CONDA_DIR}
 git clone https://github.com/HerculesJack/assignment-1910
 cd assignment-1910
-bash install.sh > /dev/null
+bash install.sh > /dev/null 2>&1
 rm -rf assignment-1910
