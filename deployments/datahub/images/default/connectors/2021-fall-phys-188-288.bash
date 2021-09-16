307608e62f4e7139fbdb136cf172ceee419e36ae
Balaji Alwar <balajialwar@berkeley.edu>
2021-09-16 11:42:10 -0700

    Update deployments/datahub/images/default/connectors/2021-fall-phys-188-288.bash
    Co-authored-by: Yuvi Panda <yuvipanda@gmail.com>

--- deployments/datahub/images/default/connectors/2021-fall-phys-188-288.bash
+++ deployments/datahub/images/default/connectors/2021-fall-phys-188-288.bash
@@ -4,5 +4,5 @@ set -euo pipefail
 cd ${CONDA_DIR}
 git clone https://github.com/HerculesJack/assignment-1910
 cd assignment-1910
-bash install.sh
+bash install.sh > /dev/null
 rm -rf assignment-1910
