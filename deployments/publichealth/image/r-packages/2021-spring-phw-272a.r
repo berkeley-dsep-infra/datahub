58aa2932fc7c08e09ec2ced0aada9dbda5174ca8
YuviPanda <yuvipanda@gmail.com>
2022-01-04 12:33:06 +0530

    Move ph272A from main hub to publichealth hub

--- deployments/publichealth/image/r-packages/2021-spring-phw-272a.r
+++ deployments/publichealth/image/r-packages/2021-spring-phw-272a.r
@@ -5,7 +5,7 @@ source("/tmp/class-libs.R")
 class_name = "PHW 272a Spring 2021"
 class_libs = c(
   "ggmap", "3.0.0",
-  "tmap", "3.2",
-  "rgdal", "1.5-39"
+  "tmap", "3.3-2",
+  "rgdal", "1.5-28"
 )
 class_libs_install_version(class_name, class_libs)
