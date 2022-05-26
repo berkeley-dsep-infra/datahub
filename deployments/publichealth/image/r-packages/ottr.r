0121de8fc9f1dab1fc759466e94c22f5c64dea59
Jonathan Felder <felder@berkeley.edu>
2022-05-26 13:26:54 -0700

    Ottr round 2.

--- deployments/publichealth/image/r-packages/ottr.r
+++ deployments/publichealth/image/r-packages/ottr.r
@@ -1,15 +1,6 @@
 #!/usr/bin/env Rscript
 # From https://github.com/berkeley-dsep-infra/datahub/issues/3403
+
 print("Installing packages for ottr")
-
-source("/tmp/class-libs.R")
-
-class_name = "ottr"
-
-class_libs = c(
-  "ottr", "1.1.4"
-)
-
-class_libs_install_version(class_name, class_libs)
-
+devtools::install_version("ottr", version = "1.1.4", repos = "https://cran.r-project.org", upgrade = "never", quiet = FALSE)
 print("Done installing packages for ottr")
