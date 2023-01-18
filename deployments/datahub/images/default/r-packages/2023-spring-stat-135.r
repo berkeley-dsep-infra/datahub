1a91e846c081227a81527971ab41e93dbd5a7220
ryanlovett <rylo@berkeley.edu>
2023-01-17 21:15:45 -0800

    Fix minor version.

--- deployments/datahub/images/default/r-packages/2023-spring-stat-135.r
+++ deployments/datahub/images/default/r-packages/2023-spring-stat-135.r
@@ -4,6 +4,6 @@ source("/tmp/class-libs.R")
 
 class_name = "Stat 135 Spring 2023"
 class_libs = c(
-  "mosaicData", "0.23.3"
+  "mosaicData", "0.20.3"
 )
 class_libs_install_version(class_name, class_libs)
