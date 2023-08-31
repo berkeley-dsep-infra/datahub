30ebb0a5451ce8087019fdee0abcd76bd157a8c1
ryanlovett <rylo@berkeley.edu>
2023-08-30 23:31:12 -0700

    Add R packages for MBA 247.

---
+++ deployments/datahub/images/default/r-packages/2023-fall-mba-247.r
@@ -0,0 +1,17 @@
+#!/usr/bin/env Rscript
+
+source("/tmp/class-libs.R")
+
+class_name = "MBA 247 Fall 2023"
+class_libs = c(
+  "caret", "6.0-94",
+  "arules", "1.7-6",
+  "arulesViz", "1.5-2",
+  "h2o", "3.42.0.2",
+  "tm", "0.7-11",
+  "SnowballC", "0.7.1",
+  "wordcloud", "2.6",
+  "pROC", "1.18.4",
+  "rpart.plot", "3.1.1"
+)
+class_libs_install_version(class_name, class_libs)
