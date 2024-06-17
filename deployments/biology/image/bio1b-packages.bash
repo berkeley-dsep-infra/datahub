98508ac95af555522f7a02fac39867482e660b77
ryanlovett <rylo@berkeley.edu>
2024-06-17 09:47:39 -0700

    Bump and disable paup.

--- deployments/biology/image/bio1b-packages.bash
+++ deployments/biology/image/bio1b-packages.bash
@@ -1,5 +1,11 @@
 # Install PAUP* for BIO 1B
 # https://github.com/berkeley-dsep-infra/datahub/issues/1699
-wget http://phylosolutions.com/paup-test/paup4a168_ubuntu64.gz -O ${CONDA_DIR}/bin/paup.gz
+
+# This package was requested in 2020 for the instructor to try out.
+# The 168 version doesn't exist so I've bumped it to 169, but also disabled
+# it in case the package is no longer needed.
+return
+
+wget https://phylosolutions.com/paup-test/paup4a169_ubuntu64.gz -O ${CONDA_DIR}/bin/paup.gz
 gunzip ${CONDA_DIR}/bin/paup.gz
 chmod +x ${CONDA_DIR}/bin/paup
